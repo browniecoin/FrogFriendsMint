0d8a64cb8a2aead09bb2a1be51bd48d97bd78671
Brown Brownie <browniecoinsdev@gmail.com>
2023-10-26 17:54:14 -0700

    update

---
+++ src/components/ConnectMetamask/ConnectToMetamask.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState, useRef } from "react";
+import metamaskIcon from "./metamask.svg";
+import Web3 from 'web3';
+import { v4 as uuidv4 } from 'uuid'; // Import the v4 function from the uuid library
+// import { Entity, Scene } from "aframe-react";
+import "./styles.css";
+
+import axios from 'axios';
+import * as d3 from 'd3';
+
+const ConnectToMetamask = ({ connectToMetamask }) => {
+  const [value, setValue] = useState('');
+
+
+
+  return (
+    <div className="container" >
+      <div className="jumbotron custom-jumbotron">
+        <h1 className="display-5 text-center">
+          Frog Friends
+        </h1>
+        <h3 className="display-5 p-4 text-center">
+          PEPE the Frog Friends
+        </h3>
+
+        <img src="images/ff.gif" width="100%" />
+
+
+
+                    <hr className="my-4" />
+                    <button
+                      onClick={connectToMetamask}
+                      className="btn btn-primary d-flex align-items-center"
+                      style={{ fontSize: "0.9rem", letterSpacing: "0.14rem" }}
+                    >
+                      Mint
+                    </button>
+
+
+
+        <hr className="my-2" />
+        Frog Friends &copy; 2023 All rights reserved.
+        <hr className="my-2" />
+      </div>
+    </div>
+  );
+};
+
+export default ConnectToMetamask;
