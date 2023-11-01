bc3368f59e3f77113395c737c76bcdfdac60aefb
Brown Brownie <browniecoinsdev@gmail.com>
2023-10-31 21:37:13 -0700

    update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,18 +7,13 @@ const Navbar = () => {
     <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
       <div className="container">
         <Link to="/" className="navbar-brand ml-2">
-          BrownieCoins.org
+          PEPE
         </Link>
         <div id="navbar" className="navbar">
           <ul
             style={{ fontSize: "0.8rem", letterSpacing: "0.2rem" }}
             className="navbar-nav ml-auto"
           >
-          <li className="nav-item">
-            <Link to="/mint" className="nav-link">
-              Mint
-            </Link>
-          </li>
           <li className="nav-item">
             <Link to="/opensea" className="nav-link">
               Opensea
